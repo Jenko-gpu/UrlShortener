e728a003d685bea8e51415d198284450bde8606c
Jenko-gpu <74445915+Jenko-gpu@users.noreply.github.com>
2021-10-22 20:10:03 +0500

    Updated models

--- UrlShortener/models.py
+++ UrlShortener/models.py
@@ -5,7 +5,7 @@ class Urls(models.Model):
     shortenedUrl = models.CharField(verbose_name="shortenedUrl", max_length=256)
     urlToShorten = models.CharField(verbose_name="urlToShorten", max_length=256)
     views = models.IntegerField(verbose_name="views")
-    rand_id = models.IntegerField(verbose_name="rand_id", db_index=True)
+    rand_id = models.IntegerField(verbose_name="rand_id", db_index=True, unique=True)
 
     def __str__(self):
         return self.name
